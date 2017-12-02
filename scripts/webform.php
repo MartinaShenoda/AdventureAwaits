8c8c941550316aee83b88e90d20670f89f10b948
MartinaShenoda <martina.f.shenoda@gmail.com>
2017-12-01 21:19:00 -0600

    Add files via upload

---
+++ scripts/webform.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html>
+<body>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?><br>
+Your feeling is: <?php echo $_POST["feeling"]; ?><br>
+</body>
+</html>
